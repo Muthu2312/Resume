1bf41eafd392eafdb7c6e58866055cebe8580485
Muthu2312 <mutharasansword@gmail.com>
2024-07-15 19:16:13 +0530

    finall

--- Project/wsgi.py
+++ Project/wsgi.py
@@ -1,21 +1,3 @@
-"""
-WSGI config for Project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-# import os
-# from django.core.wsgi import get_wsgi_application
-# from whitenoise import WhiteNoise
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
-
-# application = get_wsgi_application()
-# application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -23,3 +5,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
 application = get_wsgi_application()
+
+app = application
